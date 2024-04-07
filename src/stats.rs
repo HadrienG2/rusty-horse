7bec6dd455c855c1035d48fc7dc22a9a82f11e23
Hadrien G. <knights_of_ni@gmx.com>
2024-04-07 09:39:46 +0200

    Clean up ngram stats + more logical order

--- src/stats.rs
+++ src/stats.rs
@@ -137,101 +137,6 @@ impl FileStatsBuilder {
 /// Cumulative knowledge from a data file
 pub type FileStats = HashMap<UniCase<Ngram>, CaseStats>;
 
-/// Cumulative knowledge about an n-gram
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
-pub struct NgramStats {
-    /// Year of first occurence
-    first_year: Year,
-
-    /// Year of last occurence
-    last_year: Year,
-
-    /// Total number of matches over period of interest
-    match_count: NonZeroUsize,
-
-    /// Lower bound on the number of books with matches over period of interest
-    ///
-    /// Is an exact count when the stats only cover a single n-gram casing, but
-    /// becomes a lower bound as soon as case classes are merged.
-    min_volume_count: NonZeroUsize,
-}
-//
-impl NgramStats {
-    /// Set up statistics from a single dataset entry
-    pub fn new(entry: Entry) -> Self {
-        Self {
-            first_year: entry.year,
-            last_year: entry.year,
-            match_count: entry.match_count,
-            min_volume_count: entry.volume_count,
-        }
-    }
-
-    /// Update statistics with a new yearly entry
-    ///
-    /// Yearly entries should be merged in the order that they appear in data
-    /// files, i.e. from least recent to most recent.
-    pub fn add_entry(&mut self, entry: Entry) {
-        debug_assert!(
-            self.first_year <= self.last_year,
-            "Violated first < last year type invariant"
-        );
-        assert!(
-            entry.year > self.first_year.max(self.last_year),
-            "Dataset entries should be sorted by year"
-        );
-        self.last_year = entry.year;
-        self.match_count = add_nonzero_usize(self.match_count, entry.match_count);
-        // We can add volume counts here because we're still case-sensitive at
-        // this point in time and books only have one publication date so there
-        // is no aliasing between different years)
-        self.min_volume_count = add_nonzero_usize(self.min_volume_count, entry.volume_count);
-    }
-
-    /// Merge statistics from two case-equivalent ngrams*
-    ///
-    /// Once you start doing this, you should stop calling add_entry.
-    pub fn merge(&mut self, rhs: NgramStats) {
-        self.first_year = self.first_year.min(rhs.first_year);
-        self.last_year = self.last_year.max(rhs.last_year);
-        self.match_count = add_nonzero_usize(self.match_count, rhs.match_count);
-        // We cannot just add volume counts here because different casings of
-        // the same word may appear within the same book
-        self.min_volume_count = self.min_volume_count.max(rhs.min_volume_count);
-    }
-}
-//
-impl Ord for NgramStats {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match self.match_count.cmp(&other.match_count) {
-            Ordering::Greater => return Ordering::Greater,
-            Ordering::Less => return Ordering::Less,
-            Ordering::Equal => {}
-        }
-        match self.min_volume_count.cmp(&other.min_volume_count) {
-            Ordering::Greater => return Ordering::Greater,
-            Ordering::Less => return Ordering::Less,
-            Ordering::Equal => {}
-        }
-        match self.last_year.cmp(&other.last_year) {
-            Ordering::Greater => return Ordering::Greater,
-            Ordering::Less => return Ordering::Less,
-            Ordering::Equal => {}
-        }
-        match self.first_year.cmp(&other.first_year) {
-            Ordering::Less => Ordering::Greater,
-            Ordering::Greater => Ordering::Less,
-            Ordering::Equal => Ordering::Equal,
-        }
-    }
-}
-//
-impl PartialOrd for NgramStats {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
 /// Cumulative knowledge about an n-gram case equivalence class
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct CaseStats {
@@ -271,7 +176,7 @@ impl CaseStats {
         );
 
         // Add n-gram statistics to the case-equivalent statistics
-        self.total_stats.merge(stats);
+        self.total_stats.merge_cases(stats);
 
         // If this n-gram has better stats than the previous top ngram, it
         // becomes the new top n-gram.
@@ -290,12 +195,119 @@ impl CaseStats {
             self.total_stats >= self.top_stats,
             "total_stats should integrate the top casing's stats and then some"
         );
-        self.total_stats.merge(rhs.total_stats);
+        self.total_stats.merge_cases(rhs.total_stats);
         if rhs.top_ngram == self.top_ngram {
-            self.top_stats.merge(rhs.top_stats);
+            self.top_stats.merge_cases(rhs.top_stats);
         } else if rhs.top_stats > self.top_stats {
             self.top_ngram = rhs.top_ngram;
             self.top_stats = rhs.top_stats;
         }
+    }
+}
+
+/// Cumulative knowledge about an n-gram
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct NgramStats {
+    /// Year of first occurence
+    first_year: Year,
+
+    /// Year of last occurence
+    last_year: Year,
+
+    /// Total number of matches over period of interest
+    match_count: NonZeroUsize,
+
+    /// Lower bound on the number of books with matches over period of interest
+    ///
+    /// Is an exact count as long as the stats only cover a single n-gram
+    /// casing, but becomes a lower bound when equivalent casing are merged.
+    min_volume_count: NonZeroUsize,
+}
+//
+impl NgramStats {
+    /// Set up statistics from a single dataset entry
+    pub fn new(entry: Entry) -> Self {
+        Self::validate_entry(&entry);
+        Self {
+            first_year: entry.year,
+            last_year: entry.year,
+            match_count: entry.match_count,
+            min_volume_count: entry.volume_count,
+        }
+    }
+
+    /// Update statistics with a new yearly entry
+    ///
+    /// Yearly entries should be merged in the order that they appear in data
+    /// files, i.e. from least recent to most recent.
+    pub fn add_entry(&mut self, entry: Entry) {
+        debug_assert!(
+            self.first_year <= self.last_year,
+            "Violated first < last year type invariant"
+        );
+        Self::validate_entry(&entry);
+        assert!(
+            entry.year > self.first_year.max(self.last_year),
+            "Dataset entries should be sorted by year"
+        );
+        self.last_year = entry.year;
+        self.match_count = add_nonzero_usize(self.match_count, entry.match_count);
+        // We can add volume counts here because we're still case-sensitive at
+        // this point in time and books only have one publication date so
+        // different year entries won't refer to the same book.
+        self.min_volume_count = add_nonzero_usize(self.min_volume_count, entry.volume_count);
+    }
+
+    /// Merge statistics from two case-equivalent ngrams
+    ///
+    /// You should only do this after you're done accumulating data for the
+    /// current ngram
+    pub fn merge_cases(&mut self, rhs: NgramStats) {
+        self.first_year = self.first_year.min(rhs.first_year);
+        self.last_year = self.last_year.max(rhs.last_year);
+        self.match_count = add_nonzero_usize(self.match_count, rhs.match_count);
+        // We cannot add volume counts here because different casings of the
+        // same word may appear within the same book, so we must take a
+        // pessimistic lower bound.
+        self.min_volume_count = self.min_volume_count.max(rhs.min_volume_count);
+    }
+
+    /// Make sure that an entry is reasonable
+    fn validate_entry(entry: &Entry) {
+        assert!(
+            entry.match_count >= entry.volume_count,
+            "Entry cannot appear in more books than it has matches"
+        );
+    }
+}
+//
+impl Ord for NgramStats {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.match_count.cmp(&other.match_count) {
+            Ordering::Greater => return Ordering::Greater,
+            Ordering::Less => return Ordering::Less,
+            Ordering::Equal => {}
+        }
+        match self.min_volume_count.cmp(&other.min_volume_count) {
+            Ordering::Greater => return Ordering::Greater,
+            Ordering::Less => return Ordering::Less,
+            Ordering::Equal => {}
+        }
+        match self.last_year.cmp(&other.last_year) {
+            Ordering::Greater => return Ordering::Greater,
+            Ordering::Less => return Ordering::Less,
+            Ordering::Equal => {}
+        }
+        match self.first_year.cmp(&other.first_year) {
+            Ordering::Less => Ordering::Greater,
+            Ordering::Greater => Ordering::Less,
+            Ordering::Equal => Ordering::Equal,
+        }
+    }
+}
+//
+impl PartialOrd for NgramStats {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
